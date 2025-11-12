f410200122d3d08435de01773d672b8864fd3e32
twobeass <t.nickels@gmail.com>
2025-11-12 15:32:12 +0100

    Bump version from 0.2.1 to 0.3.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / "README.md").read_text(encoding='utf-8')
 
 setup(
     name='visiowings',
-    version='0.2.1',
+    version='0.3.0',
     description='VBA Editor for Microsoft Visio with VS Code integration',
     long_description=long_description,
     long_description_content_type='text/markdown',
