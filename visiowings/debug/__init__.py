db805c4b5289cbdffe2ad3166d8eeb1ba8e62e20
twobeass <t.nickels@gmail.com>
2025-11-15 19:49:20 +0100

    feat: Update debug module exports with new components

--- visiowings/debug/__init__.py
+++ visiowings/debug/__init__.py
@@ -8,10 +8,35 @@ from .debug_adapter import VisioDebugAdapter
 from .debug_session import DebugSession
 from .com_bridge import COMBridge
 from .breakpoint_manager import BreakpointManager
+from .event_monitor import VBAEventMonitor, VBAExecutionMode
+from .variable_inspector import VariableInspector
+from .call_stack import CallStackInspector
+from .error_handler import (
+    ErrorHandler,
+    BreakpointCleanupManager,
+    RecoveryManager,
+    DebugError,
+    COMError,
+    BreakpointError,
+    SessionError,
+)
 
 __all__ = [
     'VisioDebugAdapter',
     'DebugSession',
     'COMBridge',
     'BreakpointManager',
+    'VBAEventMonitor',
+    'VBAExecutionMode',
+    'VariableInspector',
+    'CallStackInspector',
+    'ErrorHandler',
+    'BreakpointCleanupManager',
+    'RecoveryManager',
+    'DebugError',
+    'COMError',
+    'BreakpointError',
+    'SessionError',
 ]
+
+__version__ = '0.1.0'
